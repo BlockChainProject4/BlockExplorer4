70b290b143e6c01bd123173b2ced0960fd55727d
junghyunB <bjh940126@gmail.com>
2022-05-13 16:44:44 +0900

    ds

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -8,21 +8,31 @@ const Index = () => {
     const [data, setData] = useState({
         data:"123"
     })
+    const [mineCount, setMineCount] = useState()
+    const handleChange = (e) => {
+        let {name, value} = e.target;
+        
+        setMineCount({
+            ...mineCount,
+            value
+        });
+    };
+
+
 
     const handleClick = async () => {
-        setInterval(() => {
 
-        }, 1000, 5);
-
-        await axios.post('http://localhost:3001/mineBlock', data)
-        console.log(data);
-        // alert(result.data)
+        for(let i = 0; i < mineCount.value; i++ ) {
+ 
+       await axios.post('http://localhost:3001/mineBlock', data)
     }
+}
+    
 
     return(
         <div>
+        <input placeholder='채굴 횟수를 입력 해 주세요..' type="number" onChange={handleChange}/>
         <Button variant="danger" onClick={handleClick}>채굴버튼</Button>
-
         </div>
         // <button onClick={handleClick}>채굴</button>
         
