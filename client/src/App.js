33c256a4a244af613e50cf1397d898decd01fb0e
goldenoriole34 <83339731+goldenoriole34@users.noreply.github.com>
2022-05-16 16:15:38 +0900

    useCookie

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,6 @@ import Loginpage from './pages/Loginpage';
 import Mypage from './pages/Mypage';
 import CreateWallet from './pages/CreateWallet';
 
-
 function App() {
     return (
         <div className='container'>
