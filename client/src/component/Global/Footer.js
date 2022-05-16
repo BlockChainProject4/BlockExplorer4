14cffca252ccd7b2eda57edf289e20fa1bcafafc
taehhh8 <taehyun8534@gmail.com>
2022-05-16 16:28:55 +0900

    5/16 디자인수정
    5/16 디자인수정

--- client/src/component/Global/Footer.js
+++ client/src/component/Global/Footer.js
@@ -9,10 +9,18 @@ const Footer = () => {
     return (
         <div className='FooterContainer'>
             <div className='footericons'>
-                <GoMarkGithub className='icon' size={50} color="white" href="https://github.com/BlockChainProject4/BlockExplorer4.git"/> 
-                <AiTwotoneMail className='icon' size={50} color="white"/>
-                <SiGoogledrive className='icon' size={50} color="white"/>
-                <SiNotion className='icon' size={50} color="white"/>
+                <a href="https://github.com/BlockChainProject4/BlockExplorer4">
+                    <GoMarkGithub className='icon' size={50} color="white" /> 
+                </a>
+                <a href=''>
+                    <AiTwotoneMail className='icon' size={50} color="white"/>
+                </a>
+                <a href='https://docs.google.com/spreadsheets/d/1QRMdZx5hmdrHyuGeQrMnQL9oViOkSFjzAdwR6ssaXW8/edit?usp=sharing'>
+                    <SiGoogledrive className='icon' size={50} color="white"/>
+                </a>
+                <a href='https://aluminum-alder-da8.notion.site/bd6f99e4052e4677848d335fdadd28ea?v=6965b00a228d42afabc63bf4999bd8e4'>
+                    <SiNotion className='icon' size={50} color="white"/>
+                </a>
             </div>
             <div className='footertext'>
                 <b>@ 2022 BlockChain. Explore. Good</b>
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,6 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios'
 import {React, useState} from 'react';
 import './index.css'
+// import { TextField } from "@material-ui/core";
+import TextField from '@mui/material/TextField';
 
 const Index = () => {
 
@@ -60,22 +62,35 @@ const Index = () => {
     const BlockInfo = "블록 번호 : " + bringData.idx + "\n" + "블록 생성 시간 : " + bringData.timestamps + "\n" + "블록 해시 : " + bringData.hashs + "\n" + "이전 블록 해시 : " + bringData.previousHash + "\n"+ "채굴 난이도 : " + bringData.difficulty + "\n" + "Nonce 값 : " + bringData.nonce
 
     return(
-        <>
-            <span>
-            <input className='coinnumber' placeholder='채굴 횟수를 입력 해 주세요' type="number" onChange={handleChange_MINE}/>
-            <Button size="lg" variant="dark" onClick={handleClick_MINE}>채굴버튼</Button>
-            </span>
-            <span>채굴 한 블록 정보 : <textarea readOnly rows="1" name='mineBlockInfo' value="{}"></textarea></span>
-
-            <br />
-
-            <span>
-            <input placeholder='조회 할 BLOCK 정보를 입력하세요' type="text" name="idxNum" onChange={handleChange_VIEW}/>
-            <Button size="lg" variant="dark" onClick={handleClick_VIEW}>조회버튼</Button>
-            </span>
-            <span>조회 한 블록 정보 : <textarea readOnly rows="1" name='viewBlockInfo' value={bringData.idx == undefined ? "1" : BlockInfo}></textarea></span>
-
-        </>       
+        <div>
+            <div>
+                <div className='maintitle'>
+                    <h1>Create BLOCK</h1>
+                </div>
+            </div>
+            <div className='maincontainer'>
+                <div className='coinminingcontainer'>
+                    <div>
+                        <TextField id="standard-basic"  color="secondary" type="number" label="MINING NUMBER" variant="standard" onChange={handleChange_MINE}  />
+                        <Button size="lg" variant="dark" onClick={handleClick_MINE}>MINING</Button>
+                    </div>
+                    <div className='textinfo'>
+                        <p> About Mined Blocks </p> 
+                    </div>
+                    <div><textarea className='blockinfo' readOnly rows="1" name='mineBlockInfo' value="{}"></textarea></div>
+                </div>
+                <div className='blockinfocontainer'>
+                    <div>
+                        <TextField id="standard-basic"  color="secondary" type="text"  name="idxNum" label="CHECK BLOCK INFO" variant="standard" onChange={handleChange_VIEW}  />
+                        <Button size="lg" variant="dark" onClick={handleClick_VIEW}>CHECK</Button>
+                    </div>
+                    <div className='textinfo' > 
+                        <p>Viewed block information</p>
+                    </div>
+                    <div> <textarea className='blockinfo' readOnly rows="1" name='viewBlockInfo' value={bringData.idx == undefined ? "1" : BlockInfo}></textarea></div>
+                </div>
+            </div>    
+        </div>   
     )
 }
 
