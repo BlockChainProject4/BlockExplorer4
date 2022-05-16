dd6a24df98af4e604db299a4d1e6279283299426
taehhh8 <taehyun8534@gmail.com>
2022-05-16 11:40:06 +0900

    5/16작업
    5/16작업

--- client/src/component/Global/Footer.js
+++ client/src/component/Global/Footer.js
@@ -2,19 +2,20 @@ import React from 'react'
 import './Footer.css'
 import { GoMarkGithub } from "react-icons/go";
 import { AiTwotoneMail } from "react-icons/ai";
+import { SiGoogledrive } from "react-icons/si";
+import { SiNotion } from "react-icons/si";
 
 const Footer = () => {
     return (
         <div className='FooterContainer'>
-            <div>
-                <p>4조</p>
-                <p>이혜인 변정현 박태현</p>
-            </div>
-            <div>
-            </div>
             <div className='footericons'>
-                <GoMarkGithub className='giticon' size={30} color="black" href="https://github.com/BlockChainProject4/BlockExplorer4.git"/> <br></br>
-                <AiTwotoneMail className='emailicon' size={30} color="black"/>
+                <GoMarkGithub className='icon' size={50} color="white" href="https://github.com/BlockChainProject4/BlockExplorer4.git"/> 
+                <AiTwotoneMail className='icon' size={50} color="white"/>
+                <SiGoogledrive className='icon' size={50} color="white"/>
+                <SiNotion className='icon' size={50} color="white"/>
+            </div>
+            <div className='footertext'>
+                <b>@ 2022 BlockChain. Explore. Good</b>
             </div>
         </div>
     )
--- client/src/component/Global/Header.js
+++ client/src/component/Global/Header.js
@@ -2,18 +2,22 @@ import React from 'react'
 import './Header.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Container, Nav } from 'react-bootstrap';
+import { SiHiveBlockchain } from "react-icons/si";
+import { Spinner} from 'react-bootstrap';
 
 const Header = () => {
     return (
         <div className='Wrapper'>
             <header>
-            <Navbar bg="primary" variant="dark">
+            <Navbar  variant="dark">
                 <Container>
-                <Navbar.Brand Link="/">Block Explore</Navbar.Brand>
+                < SiHiveBlockchain className='footericon' color='white' size={30} />
+                <Navbar.Brand className='navtext' href="/">Block Explore</Navbar.Brand>
                 <Nav className="me-auto">
                     <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/login">LoginPage</Nav.Link>
-                    <Nav.Link href="/mypage">MyPage</Nav.Link>
+                    <Nav.Link href="/login">Search</Nav.Link>
+                    <Nav.Link href="/mypage">SearchResult</Nav.Link>
+                    <Spinner className='navspinner' animation="border" variant="light" />
                 </Nav>
                 </Container>
         </Navbar>
